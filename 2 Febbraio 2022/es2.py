2485a54add95382ee9efa1d1b41f4691031ce6b7
Luigi11162 <luigibacco01@gmail.com>
2022-11-06 12:01:08 +0100

    Migliorato esercizio2.py

---
+++ 2 Febbraio 2022/es2.py
@@ -0,0 +1,23 @@
+
+
+def decoraClasse(cls):
+    oldInit =cls.__init__
+    def newInit(self,*args,**kwargs):
+        self.tupla=([arg for arg in args],["{0}={1}".format(key,value) for key,value in kwargs.items()])
+        oldInit(self,*args,**kwargs)
+    cls.__init__=newInit
+
+    def conQualiArgomenti(self):
+            return self.tupla
+    setattr(cls,"conQualiArgomenti",conQualiArgomenti)
+
+    return cls
+
+@decoraClasse
+class C():
+    def __init__(self,*args,**kwargs):
+        pass
+
+c=C(10,"a","b",*[9,11],k1=3,k2="pop",**{"k3":"pippo"})
+
+print(c.conQualiArgomenti())
